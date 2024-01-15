1964886246348c56d978011dc65e9a6e5f2a7664
alinurammar <aalinur@stanford.edu>
2024-01-15 17:37:55 -0500

    wip: created .env file and debugging

--- frontend/recipe-creator/src/App.tsx
+++ frontend/recipe-creator/src/App.tsx
@@ -4,15 +4,20 @@ import RecipeLayout from './components/RecipeLayout'
 import IngredientInput from './components/IngredientInput';
 import LoadingIcon from './components/LoadingIcon';
 import './App.css';
+// import dotenv from 'dotenv';
+// dotenv.config();
 
 function App() {
   const [recipeList, setRecipeList] = useState<any>([]);
   const [loading, setLoading] = useState(false);
-
+  const port = process.env.PORT;
+  const databaseUrl = process.env.DATABASE_URL;
   const handleEnterPress = (ingredientList: string,  checkedCheckboxes: string) => {
     console.log('Enter key pressed. Sending Ingredients to Backend.');
     // Add the action you want to perform on "Enter" key press
-    const apiUrl = 'http://127.0.0.1:5000/ingredients';
+    const apiUrl = databaseUrl + ':' + port + '/ingredients';
+    // const apiUrl = 'http://127.0.0.1:5000/ingredients';
+    console.log(apiUrl)
     setLoading(true);
     axios.post(apiUrl , { ingredients: ingredientList, filters: checkedCheckboxes })
       .then(response => {
