9daacf0174dded67e11f86c2121f1d356be7ce76
alinurammar <aalinur@stanford.edu>
2024-01-15 22:32:52 -0500

    Create home page

--- frontend/recipe-creator/src/App.tsx
+++ frontend/recipe-creator/src/App.tsx
@@ -3,19 +3,21 @@ import axios from 'axios';
 import RecipeLayout from './components/RecipeLayout'
 import IngredientInput from './components/IngredientInput';
 import LoadingIcon from './components/LoadingIcon';
+import HomePage from './components/HomePage';
 import './App.css';
 
 function App() {
   const [recipeList, setRecipeList] = useState<any>([]);
   const [loading, setLoading] = useState(false);
+  const [isHomePage, setIsHomePage] = useState(true);
 
   const handleEnterPress = (ingredientList: string,  checkedCheckboxes: string) => {
     console.log('Enter key pressed. Sending Ingredients to Backend.');
-    // Add the action you want to perform on "Enter" key press
+// Add the action you want to perform on "Enter" key press
     const apiUrl = process.env.REACT_APP_API_URL;
-    if (typeof apiUrl != 'string') {
+    if (typeof apiUrl !== 'string') {
       console.error('Error fetching backend server:');
-      return
+      return;
     }
     setLoading(true);
     axios.post(apiUrl , { ingredients: ingredientList, filters: checkedCheckboxes })
@@ -25,7 +27,7 @@ function App() {
           let responseData: any[] = JSON.parse(responseDataString);
           if (Array.isArray(responseData)) {
             setRecipeList(responseData);
-          }
+                      }
         } catch (error) {
           console.error('Error parsing JSON:', error);
         } finally {
@@ -36,14 +38,25 @@ function App() {
         console.error('Error sending data to the backend:', error);
         setLoading(false);
       });
-
   };
+
+  const toggleHomePage = () => {
+    setIsHomePage(!isHomePage);
+  };
+
   return (
     <div>
-      <h1>Recipe App</h1>
-      <IngredientInput onEnterPress={handleEnterPress} />
-      {loading && <LoadingIcon/>}
-      <RecipeLayout recipes={recipeList} />
+      <button onClick={toggleHomePage}>{isHomePage ? 'Go to App' : 'Go to Home'}</button>
+      {isHomePage ? (
+        <HomePage />
+      ) : (
+        <div>
+          <h1>Recipe App</h1>
+          <IngredientInput onEnterPress={handleEnterPress} />
+          {loading && <LoadingIcon/>}
+          <RecipeLayout recipes={recipeList} />
+        </div>
+      )}
     </div>
   );
 }
