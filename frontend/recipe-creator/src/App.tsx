9a4a6f316129a9dc0181a84a1aef7f3262a8885e
alinurammar <aalinur@stanford.edu>
2024-02-10 16:47:54 -0500

    Update API Request to have proper configs

--- frontend/recipe-creator/src/App.tsx
+++ frontend/recipe-creator/src/App.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import RecipeGenerator from './pages/RecipeGenerator';
