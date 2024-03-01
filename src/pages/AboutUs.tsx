55b93ce161fb35d613f6278f94ef154a7d9a6326
alinurammar <aalinur@stanford.edu>
2024-02-29 23:09:50 -0500

    Add our github and linkedin profiles

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -6,6 +6,9 @@ import imadPfp from '../assets/Imad_Profile.jpg';
 import akramPfp from '../assets/Akram_Profile.jpeg';
 import imranPfp from '../assets/Imran_Profile.jpg';
 
+const LinkedInIcon = <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/LinkedIn_icon.svg/2048px-LinkedIn_icon.svg.png' alt='LinkedIn'></img>
+const GitHubIcon = <img src='https://cdn-icons-png.flaticon.com/512/25/25231.png' alt='LinkedIn'></img>
+
 const AboutUs = () => {
     return (
         <div className='container'>
@@ -48,21 +51,50 @@ const AboutUs = () => {
                         <img src={ammarPfp} alt="Ammar Alinur" />
                         <div className="name">Ammar Alinur</div>
                         <div className="position">Coder</div>
+                        <div className="social-links">
+                            <a href="https://www.linkedin.com/in/ammar-alinur/" target="_blank" rel="noopener noreferrer">
+                                <div className="social-icon">{LinkedInIcon}</div>
+                            </a>
+                            <a href="https://github.com/alinurammar" target="_blank" rel="noopener noreferrer">
+                                <div className="social-icon">{GitHubIcon} </div>
+                            </a>
+                        </div>
                     </div>
                     <div className="member">
                         <img src={imadPfp} alt="Imad Hussein" />
                         <div className="name">Imad Hussein</div>
                         <div className="position">Aspiring Chef</div>
+                        <div className="social-links">
+                            <a href="https://www.linkedin.com/in/imad-hussein/" target="_blank" rel="noopener noreferrer">
+                                <div className="social-icon">{LinkedInIcon}</div>
+                            </a>
+                            <a href="https://github.com/imad022" target="_blank" rel="noopener noreferrer">
+                                <div className="social-icon">{GitHubIcon} </div>
+                            </a>
+                        </div>
                     </div>
                     <div className="member">
                         <img src={imranPfp} alt="Imran Hussein" />
                         <div className="name">Imran Hussein</div>
                         <div className="position">Banned From Kitchen</div>
+                        <div className="social-links">
+                            <a href="https://www.linkedin.com/in/imran-hussein/" target="_blank" rel="noopener noreferrer">
+                                <div className="social-icon">{LinkedInIcon}</div>
+                            </a>
+                            <a href="https://github.com/imranh-2" target="_blank" rel="noopener noreferrer">
+                                <div className="social-icon">{GitHubIcon} </div>
+                            </a>
+                        </div>
                     </div>
                     <div className="member">
                         <img src={akramPfp} alt="Akram Hassan" />
                         <div className="name">Akram Hassan</div>
                         <div className="position">Professional Yapper</div>
+                        <div className="social-links">
+                            <a href="https://www.linkedin.com/in/akram-hassen/" target="_blank" rel="noopener noreferrer">
+                                <div className="social-icon">{LinkedInIcon}</div>
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
